00fae47c898244f8eadbbc05514509b77a1fbffe
MxSeven <107146029+Marcos0209@users.noreply.github.com>
2024-06-07 13:44:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Os dois maiores grandes craques das últimas décadas são máquinas de fazer gols. Cristiano Ronaldo e  Messi não param de balançar as redes, sendo decisivos constantemente.",
         alternativas: [
             {
                 texto: "Isso é assustador!",
